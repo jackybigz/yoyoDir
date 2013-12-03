36d5f2d40df8f2af15ba52ed7b5d397e268302f9
Jack <jackybigz@gmail.com>
2013-12-03 10:34:04 +0000

    Added companies scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 YoyoDir::Application.routes.draw do
+  resources :companies
+
+  root to: 'companies#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
