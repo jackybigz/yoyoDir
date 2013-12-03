11630a231514f084f4fee70e2cd39b85c8eac71d
Jack <jackybigz@gmail.com>
2013-12-03 09:46:50 +0000

    Inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the project files for Jack Hampsons Yo-Yo Directory.
+
+Used to specically search throughout the yoyo universe for your optimum yoyo.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YoyoDir::Application.config.secret_key_base = 'f1ff39992263650a8c1ffcf58beea840e064917efe089c2ae2203fa910ebc51a054a3b65f88059b1994c88dbef9866cd58d1b3c872700fcb91d732b4821aaef5'
